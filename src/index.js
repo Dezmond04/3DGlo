49752f4352581786e29679b38d028d77bb72ba6a
Igor Karih <dezmond04@mail.ru>
2022-05-07 02:25:37 +0300

    added 21 lesson

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import validate from "./modules/validate";
 
 timer("10 may 2022");
 menu();
 modal();
+validate();
---
+++ src/modules/validate.js
@@ -0,0 +1,40 @@
+const validate = () => {
+  const calcItem = document.querySelectorAll(".calc-item");
+
+  const validateCalc = () => {
+    calcItem.forEach((input) => {
+      input.addEventListener("input", () => {
+        if (input.type == "text") {
+          input.value = input.value.replace(/\D+/, "");
+        }
+      });
+    });
+  };
+
+  const validateForm = () => {
+    const input = document.querySelectorAll("input");
+    input.forEach((input) => {
+      const inputId = input.id;
+      if (inputId.match(/form[0-9]-name/) !== null) {
+        input.addEventListener("input", () => {
+          input.value = input.value.replace(/[^А-Яа-я\s\-]/g, "");
+        });
+      }
+      if (inputId.match(/form[0-9]-email/) !== null) {
+        input.addEventListener("input", () => {
+          input.value = input.value.replace(/[\w\@\~\-\.\!\~\*]/g, "");
+        });
+      }
+      if (inputId.match(/form[0-9]-phone/) !== null) {
+        input.addEventListener("input", () => {
+          input.value = input.value.replace(/[^0-9\()\-]/g, "");
+        });
+      }
+    });
+  };
+
+  validateCalc();
+  validateForm();
+};
+
+export default validate;
