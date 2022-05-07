e7f630dc341b8ceb5b57805bfa6fea37aeaafd20
Igor Karih <dezmond04@mail.ru>
2022-05-07 18:05:07 +0300

    added calc module

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import modal from "./modules/modal";
 import validate from "./modules/validate";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
+import calc from "./modules/calc";
 
 timer("10 may 2022");
 menu();
@@ -12,3 +13,4 @@ modal();
 validate();
 tabs();
 slider();
+calc();
